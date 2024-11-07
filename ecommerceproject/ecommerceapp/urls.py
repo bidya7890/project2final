2bbf32e9676756706ae5aa67ee3647e161ff2af5
bidya7890 <bidya7890@gmail.com>
2024-11-07 19:04:56 +0530

    skeleton for backend and frontend

---
+++ ecommerceproject/ecommerceapp/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.urls import path
+from rest_framework import routers
+
+router = routers.DefaultRouters()
+
+
+
+
+urlpatterns = [
+    # path(),
+]
